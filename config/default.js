7ffcb57cffd2b525b0c9ccb4496211383d21626d
raphael <raphaelfournier1123@gmail.com>
2022-06-22 15:47:11 +0200

    redefine promiseLibrary for mongo driver

--- config/default.js
+++ config/default.js
@@ -55,6 +55,7 @@ module.exports = (configuration = {
 		options: { // see http://mongodb.github.io/node-mongodb-native/driver-articles/mongoclient.html
 			w: 1,
 			readPreference: "primaryPreferred",
+			promiseLibrary: require('bluebird'),
 			useUnifiedTopology: true
 		}
 	},
